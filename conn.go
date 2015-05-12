783272f2dedcd8d1fc40bae8e48c3489174a3955
nieanan [聂安安] <nieanan@xiaomi.com>
2015-05-12 16:53:42 +0800

    pool, bug fix, decreate when pingTest failed

--- conn.go
+++ conn.go
@@ -36,44 +36,13 @@ func Create(address string, maxConns int, maxIdle int) *ConnPool {
 	}
 }
 
-func (this *ConnPool) tryFree() io.Closer {
-	this.Lock()
-	defer this.Unlock()
-
-	if len(this.free) == 0 {
-		return nil
-	}
-
-	conn := this.free[0]
-	this.free = this.free[1:]
-
-	return conn
-}
-
-func (this *ConnPool) reachedMax() bool {
-	this.RLock()
-	defer this.RUnlock()
-	return this.active >= this.MaxConns
-}
-
-func (this *ConnPool) increActive() {
-	this.Lock()
-	defer this.Unlock()
-	this.active += 1
-}
-
-func (this *ConnPool) decreActive() {
-	this.Lock()
-	defer this.Unlock()
-	this.active -= 1
-}
-
 func (this *ConnPool) Get() (conn io.Closer, err error) {
 	conn = this.tryFree()
 	if conn != nil {
 		if this.TestOnBorrow {
 			err = this.Ping(conn)
 			if err != nil {
+				this.decreActive() //bug fix: 如果不加decrease, 当 free不为空 + server端重启时 就会导致active大于实际情况不符
 				conn.Close()
 				conn = this.tryFree()
 				err = nil
@@ -106,19 +75,6 @@ func (this *ConnPool) Get() (conn io.Closer, err error) {
 	return
 }
 
-func (this *ConnPool) overMaxIdle() bool {
-	this.RLock()
-	defer this.RUnlock()
-	return len(this.free) >= this.MaxIdle
-}
-
-func (this *ConnPool) ForceClose(conn io.Closer) {
-	this.decreActive()
-	if conn != nil {
-		conn.Close()
-	}
-}
-
 func (this *ConnPool) Release(conn io.Closer) {
 	if this.overMaxIdle() {
 		this.decreActive()
@@ -132,6 +88,13 @@ func (this *ConnPool) Release(conn io.Closer) {
 	}
 }
 
+func (this *ConnPool) ForceClose(conn io.Closer) {
+	this.decreActive()
+	if conn != nil {
+		conn.Close()
+	}
+}
+
 func (this *ConnPool) Destroy() {
 	this.Lock()
 	defer this.Unlock()
@@ -141,4 +104,42 @@ func (this *ConnPool) Destroy() {
 			conn.Close()
 		}
 	}
+}
+
+func (this *ConnPool) tryFree() io.Closer {
+	this.Lock()
+	defer this.Unlock()
+
+	if len(this.free) == 0 {
+		return nil
+	}
+
+	conn := this.free[0]
+	this.free = this.free[1:]
+
+	return conn
+}
+
+func (this *ConnPool) reachedMax() bool {
+	this.RLock()
+	defer this.RUnlock()
+	return this.active >= this.MaxConns
+}
+
+func (this *ConnPool) increActive() {
+	this.Lock()
+	defer this.Unlock()
+	this.active += 1
+}
+
+func (this *ConnPool) decreActive() {
+	this.Lock()
+	defer this.Unlock()
+	this.active -= 1
+}
+
+func (this *ConnPool) overMaxIdle() bool {
+	this.RLock()
+	defer this.RUnlock()
+	return len(this.free) >= this.MaxIdle
 }
